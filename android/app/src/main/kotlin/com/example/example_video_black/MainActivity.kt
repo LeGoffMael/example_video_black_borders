8fa75467e0f79ba7299b1410321477740fc1fd8c
Mael Le Goff <m.legoff@xenoimpact.com>
2021-12-16 14:58:00 +0900

    🎉 first commit

---
+++ android/app/src/main/kotlin/com/example/example_video_black/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.example_video_black
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,124 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:video_player/video_player.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter video black borders example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const VideoPickerPage(),
+    );
+  }
+}
+
+// Pick video
+class VideoPickerPage extends StatefulWidget {
+  const VideoPickerPage({Key? key}) : super(key: key);
+
+  @override
+  State<VideoPickerPage> createState() => _VideoPickerPageState();
+}
+
+class _VideoPickerPageState extends State<VideoPickerPage> {
+  final ImagePicker _picker = ImagePicker();
+
+  void _pickVideo() async {
+    final XFile? file = await _picker.pickVideo(source: ImageSource.gallery);
+    if (file != null) {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => VideoPlayerPage(video: File(file.path))));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Select video'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: _pickVideo,
+              child: const Text("Pick Video From Gallery"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// Play video
+class VideoPlayerPage extends StatefulWidget {
+  final File video;
+
+  const VideoPlayerPage({Key? key, required this.video}) : super(key: key);
+
+  @override
+  State<VideoPlayerPage> createState() => _VideoPlayerPageState();
+}
+
+class _VideoPlayerPageState extends State<VideoPlayerPage> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    _controller = VideoPlayerController.file(widget.video)
+      ..initialize().then((_) {
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        setState(() {});
+
+        _controller.play();
+        _controller.setLooping(true);
+      });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Video Player'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              _controller.value.isInitialized
+                  ? AspectRatio(
+                      aspectRatio: _controller.value.aspectRatio,
+                      child: VideoPlayer(_controller),
+                    )
+                  : Container(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
